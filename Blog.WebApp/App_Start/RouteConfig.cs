fbc830665690458cba22531654622d5cc34477f8
hasloOkon <okon.anna.m@gmail.com>
2018-03-11 21:49:49 +0100

    Prepared for login.

--- Blog.WebApp/App_Start/RouteConfig.cs
+++ Blog.WebApp/App_Start/RouteConfig.cs
@@ -10,10 +10,16 @@ namespace Blog.WebApp
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Login",
+                url: "Login",
+                defaults: new { controller = "Admin", action = "Login" }
+            );
+
+            routes.MapRoute(
                 name: "PostDetails",
                 url: "PostDetails/{year}/{month}/{postSlug}",
                 defaults: new { controller = "Blog", action = "PostDetails" },
-                constraints: new { year = @"\d{4}", month = @"\d{1,2}"}
+                constraints: new { year = @"\d{4}", month = @"\d{1,2}" }
             );
 
             routes.MapRoute(
---
+++ Blog.WebApp/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Blog.WebApp.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                if (!string.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
+                    return Redirect(returnUrl);
+
+                return RedirectToAction("Manage");
+            }
+
+            ViewBag.ReturnUrl = returnUrl;
+
+            return View();
+        }
+    }
+}
