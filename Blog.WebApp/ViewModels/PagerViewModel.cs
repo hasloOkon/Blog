ec4b84a5d60dce950fcc48a2120b343673202363
Adam <the.very.random.git.user@outlook.com>
2018-03-19 15:22:41 +0100

    Enhanced pager. Centered footer.

--- Blog.WebApp/ViewModels/PagerViewModel.cs
+++ Blog.WebApp/ViewModels/PagerViewModel.cs
@@ -4,40 +4,22 @@ namespace Blog.WebApp.ViewModels
 {
     public class PagerViewModel
     {
-        public PagerViewModel(int totalItems, int pageNumber, int pageSize = 10)
+        public PagerViewModel(int totalItems, int pageNumber, int pageSize)
         {
-            var totalPages = (int)Math.Ceiling((decimal)totalItems / (decimal)pageSize);
+            var totalPages = (int)Math.Ceiling((decimal)totalItems / pageSize);
             var currentPage = pageNumber;
-            var startPage = currentPage - 5;
-            var endPage = currentPage + 4;
+            var startPage = Math.Max(currentPage - 3, 1);
+            var endPage = Math.Min(currentPage + 3, totalPages);
 
-            if (startPage <= 0)
-            {
-                endPage -= (startPage - 1);
-                startPage = 1;
-            }
-            if (endPage > totalPages)
-            {
-                endPage = totalPages;
-                if (endPage > 10)
-                {
-                    startPage = endPage - 9;
-                }
-            }
-
-            TotalItems = totalItems;
             CurrentPage = currentPage;
-            PageSize = pageSize;
             TotalPages = totalPages;
             StartPage = startPage;
             EndPage = endPage;
         }
 
-        public int TotalItems { get; private set; }
-        public int CurrentPage { get; private set; }
-        public int PageSize { get; private set; }
-        public int TotalPages { get; private set; }
-        public int StartPage { get; private set; }
-        public int EndPage { get; private set; }
+        public int CurrentPage { get; }
+        public int TotalPages { get; }
+        public int StartPage { get; }
+        public int EndPage { get; }
     }
 }
