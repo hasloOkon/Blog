3869c0f8bbdd0bfe4856fd74a9f659fc3fcc1225
Adam <the.very.random.git.user@outlook.com>
2018-03-20 17:14:43 +0100

    Fixed slugify.

--- Blog.Core/Utility/StringExtensions.cs
+++ Blog.Core/Utility/StringExtensions.cs
@@ -1,12 +1,32 @@
-﻿namespace Blog.Core.Utility
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Blog.Core.Utility
 {
     public static class StringExtensions
     {
         public static string Slugify(this string text)
         {
             return text
+                .Trim()
                 .ToLower()
-                .Replace(' ', '_');
+                .RemoveAccents()
+                .RemoveInvalidCharacters()
+                .ReplaceWhitespacesWithUnderscores();
+        }
+
+        private static string RemoveAccents(this string text)
+        {
+            return Encoding.UTF8.GetString(Encoding.GetEncoding("ISO-8859-8").GetBytes(text));
+        }
+
+        private static string ReplaceWhitespacesWithUnderscores(this string text)
+        {
+            return Regex.Replace(text, @"\s+", "_");
+        }
+        private static string RemoveInvalidCharacters(this string text)
+        {
+            return Regex.Replace(text, @"[^a-z0-9_\s]", string.Empty);
         }
     }
 }
